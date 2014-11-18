d603b0483c10126a5ce9f1b8ce75df2baf88121a
hannahborje <hannah.borjesson@gmail.com>
2014-11-18 22:09:50 +0100

    Added backgroundImage and style

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,28 +2,25 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', [function() {
+    .controller('View1Ctrl', [function() {
+        
+    }])
 
-}])
-
-.controller('TextCtrl', ['$scope', function($scope) {
-
-        /*
-        $scope.imageSource = {
-            'background-image': "url(http://s30.postimg.org/74ic3v80h/meagain.jpg)",
-            'background': "url(http://s30.postimg.org/vd3oacj0x/CAM00109.jpg)"
-
-        }; */
-
-
+    .controller('TextCtrl', ['$scope', function($scope) {
+    
+    /*
+      $scope.imageSource = {
+      'background-image': "url(http://s30.postimg.org/74ic3v80h/meagain.jpg)",
+      'background': "url(http://s30.postimg.org/vd3oacj0x/CAM00109.jpg)"
+      
+      }; */
         $scope.yolo = "Hannah the Programmer";
 
-    }])
-;
+    }]);
